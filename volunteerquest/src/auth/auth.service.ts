2c6fcf91627b63266e7fd9c77d8fc9703292b912
Benjamin Vencill <btvencill@gmail.com>
2018-01-24 14:02:40 -0500

    Users now assigned roles. Roles are stored in firestore, and referenced at login

--- volunteerquest/src/auth/auth.service.ts
+++ volunteerquest/src/auth/auth.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
-
-import { AngularFireAuth } from 'angularfire2/auth';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
-
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { switchMap } from 'rxjs/operators';
+import { User } from './user';
+import { merge } from 'rxjs/observable/merge';
+
 
 @Injectable()
 export class AuthService {
-  user: Observable<firebase.User>;
 
-  constructor(private firebaseAuth: AngularFireAuth) {
-    this.user = firebaseAuth.authState;
+  user: Observable<User>;
+
+  constructor(private firebaseAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+              private router: Router) { 
+
+      this.user = this.firebaseAuth.authState
+        .switchMap(user => {
+          if (user) {
+            return this.afs.doc<User>(`users/${user.uid}`).valueChanges();
+          } else {
+            return Observable.of(null);
+          }
+        })  
   }
 
   signup(email: string, password: string) {
@@ -30,6 +45,7 @@ export class AuthService {
       .auth
       .signInWithEmailAndPassword(email, password)
       .then(value => {
+        this.updateUserData(value);
         console.log('Nice, it worked!');
       })
       .catch(err => {
@@ -43,4 +59,51 @@ export class AuthService {
       .signOut();
   }
 
+  private updateUserData(user) {
+    //Set user data to firestore on login
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(`users/${user.uid}`);
+    const data: User = {
+      uid: user.uid,
+      email: user.email,
+      roles: {
+        //Default accounts are subscriber only. 
+        subscriber: true,
+        //TODO: enable sign-up with token for editor privalage?
+        editor: false,
+        admin: false
+      }
+    }
+    return userRef.set(data, {merge: true}) //merge creates or updates data in non-destructive way
+  }
+
+  //Consider moving this logic to a separate class if it becomes overly complex
+  //Role-based Authorization
+  canRead(user: User): boolean {
+    const allowed = ['admin', 'editor', 'subscriber']
+    return this.checkAuthorization(user, allowed)
+  }
+  canEdit(user: User): boolean {
+    const allowed = ['admin', 'editor']
+    return this.checkAuthorization(user, allowed)
+  }
+  canDelete(user: User): boolean {
+    const allowed = ['admin']
+    return this.checkAuthorization(user, allowed)
+  }
+
+  // determines if user has matching role
+  private checkAuthorization(user: User, allowedRoles: string[]): boolean {
+    if (!user) {
+      return false;
+    } 
+    for (const role of allowedRoles) {
+      if ( user.roles[role] ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
 }
+
+
