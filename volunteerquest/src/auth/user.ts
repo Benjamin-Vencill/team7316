615bdd41f3f10d89f826ed986ca539cd994a0057
Benjamin Vencill <btvencill@gmail.com>
2018-01-24 14:09:36 -0500

    User now assigned roles: admin, editor, and/or subscriber

---
+++ volunteerquest/src/auth/user.ts
@@ -0,0 +1,11 @@
+export interface Roles { 
+    subscriber?: boolean;
+    editor?: boolean;
+    admin?: boolean;
+ }
+  
+export interface User {
+    uid: string;
+    email: string;
+    roles: Roles;
+}
