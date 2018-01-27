72d93bff1a40f6f477aa2b25c2d183c624c92d74
Chris Calloway <chris.callowayd@gmail.com>
2018-01-27 18:11:32 -0500

    Adding drop-down menu to map view page.

--- volunteerquest/src/app/app.module.ts
+++ volunteerquest/src/app/app.module.ts
@@ -4,8 +4,19 @@ import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore'
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpModule }    from '@angular/http';
+import { MatButtonModule, MatMenuModule, MatToolbarModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { NgModule } from '@angular/core';
 import { Router, Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from '../routing/app-routing.module';
@@ -37,7 +48,19 @@ import { EditPostComponent } from '../edit-post/edit-post.component';
       apiKey: GoogleMapsConfig.googleMapsAPI.apiKey.valueOf()
     }),
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatMenuModule,
+    MatSliderModule,
+    MatToolbarModule,
+    MatIconModule,
     HttpModule,
     AppRoutingModule,
     AuthModule
