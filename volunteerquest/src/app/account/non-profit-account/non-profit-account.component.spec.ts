b7c6cd1911dd4eccbdaa236a26ec55d6592504ad
Benjamin Vencill <btvencill@gmail.com>
2018-04-13 10:56:18 -0400

    Re-factor of AccountComponent to differentiate nonprofit and volunteer accounts.

---
+++ volunteerquest/src/app/account/non-profit-account/non-profit-account.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NonProfitAccountComponent } from './non-profit-account.component';
+
+describe('AccountComponent', () => {
+  let component: NonProfitAccountComponent;
+  let fixture: ComponentFixture<NonProfitAccountComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NonProfitAccountComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NonProfitAccountComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
